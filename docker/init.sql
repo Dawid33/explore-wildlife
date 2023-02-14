28cc8e599c4966b8a5f6c72bdfd178f44ac1507f
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-02-14 16:43:57 +0000

    testing

--- docker/init.sql
+++ docker/init.sql
@@ -27,5 +27,4 @@ CREATE TABLE app.comments_edges (
 );
 
 INSERT INTO app.users (display_name, email, password) VALUES ('John Doe Bro', 'jdoe@example.com', 'jdoe');
-INSERT INTO app.users (display_name, email, password) VALUES ('another', 'another@example.com', 'another');
 INSERT INTO app.users (display_name, email, password) VALUES ('test', 'test@example.com', 'test');
