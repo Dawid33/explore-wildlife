7c70def96e9cee8e64376612a00f27684addf583
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-02-17 18:39:46 +0000

    Basic image support

--- android-app/app/src/main/java/com/android/api/GetImageRequest.java
+++ android-app/app/src/main/java/com/android/api/GetImageRequest.java
@@ -1,7 +1,18 @@
 package com.android.api;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.media.Image;
+import android.media.MediaCodec;
+import android.media.MediaCodecList;
+
 import org.json.JSONArray;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.Callable;
@@ -11,12 +22,12 @@ public class GetImageRequest implements Callable<GetImageRequest.GetImageRequest
     String imageId;
 
     public class GetImageRequestResult {
-        public JSONArray posts;
+        public Image image;
         public boolean requestSucceeded;
 
-        public GetImageRequestResult(boolean requestSucceded, JSONArray posts) {
+        public GetImageRequestResult(boolean requestSucceded, Image image) {
             this.requestSucceeded = requestSucceded;
-            this.posts = posts;
+            this.image = image;
         }
     }
 
@@ -33,6 +44,15 @@ public class GetImageRequest implements Callable<GetImageRequest.GetImageRequest
             urlConnection.setDoInput(true);
             urlConnection.setChunkedStreamingMode(0);
             urlConnection.connect();
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BigInteger contentLength = new BigInteger(urlConnection.getHeaderField("content-length"));
+                System.out.println(contentLength);
+                DataInputStream s = new DataInputStream(urlConnection.getInputStream());
+                byte[] rawBytes =  new byte[contentLength.intValue()];
+                s.readFully(rawBytes);
+            } else {
+                return new GetImageRequestResult(false, null);
+            }
 
             return new GetImageRequestResult(true, null);
         } catch (Exception e) {
--- android-app/app/src/main/java/com/android/ui/app/PostsAdapter.java
+++ android-app/app/src/main/java/com/android/ui/app/PostsAdapter.java
@@ -1,33 +1,37 @@
 package com.android.ui.app;
 
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.R;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+    public static final String imageApiUrl = "https://explorewildlife.net/api/image?id=";
     JSONArray data;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        public TextView textView;
+        public ImageView imageView;
+        public View view;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-
-            textView = (TextView) view.findViewById(R.id.textView);
-        }
-
-        public TextView getTextView() {
-            return textView;
+            this.view = view;
+            textView = view.findViewById(R.id.textView);
+            imageView = view.findViewById(R.id.imageView);
         }
     }
 
@@ -41,14 +45,20 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         // Create a new view, which defines the UI of the list item
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.post_row_item, viewGroup, false);
-
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(@NonNull PostsAdapter.ViewHolder viewHolder, int position) {
         try {
-            viewHolder.getTextView().setText(data.get(position).toString());
+            viewHolder.textView.setText(data.get(position).toString());
+            try {
+                JSONArray images = ((JSONObject) data.get(position)).getJSONArray("images");
+                // Only get the first image for simplicity, there might be more in the array
+                Glide.with(viewHolder.view)
+                        .load(imageApiUrl + images.get(0))
+                        .into(viewHolder.imageView);
+            } catch (Exception ignored) {}
         } catch (JSONException e) {
             e.printStackTrace();
         }
