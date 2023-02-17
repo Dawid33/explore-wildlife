f7f99c7f69c478224f911d782ae0aa0727721681
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-02-17 19:20:22 +0000

    Added account overlay toggle button

--- android-app/app/src/main/java/com/android/AppActivity.java
+++ android-app/app/src/main/java/com/android/AppActivity.java
@@ -1,18 +1,23 @@
 package com.android;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.android.databinding.ActivityAppBinding;
+import com.android.ui.AccountFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AppActivity extends AppCompatActivity {
     private ActivityAppBinding binding;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,19 @@ public class AppActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        binding.goToAccountButton.setOnClickListener(view -> {
+            if (accountFragment == null) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                accountFragment = new AccountFragment();
+                transaction.add(binding.appBase.getId(), accountFragment).commit();
+            } else {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.remove(accountFragment).commit();
+                accountFragment = null;
+            }
+        });
     }
 }
 
-//        NavigationBarView.OnItemSelectedListener onItemSelectedListener = (MenuItem item) -> {
-//            System.out.println(item.getTitle());
-//            return true;
-//        };
-//        bottomNav.setOnItemSelectedListener(onItemSelectedListener);
--- android-app/app/src/main/java/com/android/ui/AccountFragment.java
+++ android-app/app/src/main/java/com/android/ui/AccountFragment.java
@@ -1,4 +1,4 @@
-package com.android.ui.app;
+package com.android.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
