bcaf36a1b8b5a2adf3d2ff08ece143e8fc6968bf
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-02-21 15:32:25 +0000

    Working on image upload

---
+++ android-app/app/src/main/java/com/android/api/CreatePostRequest.java
@@ -0,0 +1,76 @@
+package com.android.api;
+
+import org.json.JSONObject;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Callable;
+
+public class CreatePostRequest implements Callable<CreatePostRequest.CreatePostRequestResult> {
+
+    public static final String loginApiUrl = "https://explorewildlife.net/api/login";
+    String email, password;
+
+    public class CreatePostRequestResult {
+        public String error;
+        public boolean isLoggedIn;
+
+        public CreatePostRequestResult(boolean isLoggedIn, String error) {
+            this.error = error;
+            this.isLoggedIn = isLoggedIn;
+        }
+    }
+
+    public CreatePostRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public CreatePostRequestResult call() throws Exception {
+        HttpURLConnection urlConnection = null;
+
+        // Create Multipart form to send with the posts request.
+        String boundary = "===" + System.currentTimeMillis() + "===";
+        MultipartFormBody form = new MultipartFormBody(boundary);
+        form.addField("email", this.email);
+        form.addField("password", this.password);
+
+        try {
+            URL url = new URL(loginApiUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setChunkedStreamingMode(0);
+            urlConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            // Open a stream to write data to the request body
+            OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
+            // Write the multipart form to the body of the request
+            writer.write(form.toString());
+            writer.close();
+            urlConnection.connect();
+
+            // Read the response
+            String response = Utils.readStream(urlConnection.getInputStream());
+
+            JSONObject json = new JSONObject(response);
+            System.out.println(json);
+            if (json.has("success")) {
+                boolean success = (boolean) json.get("success");
+                if (success) {
+                    return new CreatePostRequestResult(true, "");
+                } else if (json.has("error")) {
+                    return new CreatePostRequestResult(false, (String) json.get("error"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Bypass login on error because of strange ssl issues on campus.
+//            return true;
+        } finally {
+            if (urlConnection != null) { urlConnection.disconnect(); }
+        }
+        return new CreatePostRequestResult(false, "Unknown error occurred.");
+    }
+}
--- android-app/app/src/main/java/com/android/api/MultipartFormBody.java
+++ android-app/app/src/main/java/com/android/api/MultipartFormBody.java
@@ -2,13 +2,31 @@ package com.android.api;
 
 import androidx.annotation.NonNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class MultipartFormBody {
     final String LINE_FEED = "\r\n";
-    HashMap<String, String> keys;
+    HashMap<String, Value> keys;
     String boundary;
+
+    public class Value {
+        public Value(String str) {
+            this.str = str;
+            this.path = null;
+        }
+
+        public Value(Path path) {
+            this.str = "";
+            this.path = null;
+        }
+        public String str;
+        public Path path;
+    }
 
     public MultipartFormBody(String boundary) {
         this.keys = new HashMap<>();
@@ -16,19 +34,37 @@ public class MultipartFormBody {
     }
 
     public void addField(String key, String value) {
-        this.keys.put(key, value);
+        this.keys.put(key, new Value(value));
+    }
+
+    public void addFile(String key, Path value) {
+        this.keys.put(key, new Value(value));
     }
 
     @NonNull
     public String toString() {
         StringBuilder b = new StringBuilder();
 
-        for(Entry<String, String> e : this.keys.entrySet()) {
-            b.append("--").append(this.boundary).append(LINE_FEED);
-            b.append("Content-Disposition: form-data; name=\"").append(e.getKey()).append("\"") .append(LINE_FEED);
-            b.append("Content-Type: text/plain;").append( LINE_FEED);
-            b.append(LINE_FEED);
-            b.append(e.getValue()).append(LINE_FEED);
+        for(Entry<String, Value> e : this.keys.entrySet()) {
+            if (e.getValue().path == null) {
+                b.append("--").append(this.boundary).append(LINE_FEED);
+                b.append("Content-Disposition: form-data; name=\"").append(e.getKey()).append("\"") .append(LINE_FEED);
+                b.append("Content-Type: text/plain;").append( LINE_FEED);
+                b.append(LINE_FEED);
+                b.append(e.getValue().str).append(LINE_FEED);
+            } else {
+                b.append("--").append(this.boundary).append(LINE_FEED);
+                b.append("Content-Disposition: form-data; name=\"file\"; filename=\"").append(e.getValue().path.getFileName()).append("\"").append(LINE_FEED);
+                b.append("Content-Type: application/octet-stream").append(LINE_FEED);// + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
+                b.append(LINE_FEED);
+
+                try {
+                    byte[] image = Files.readAllBytes(e.getValue().path);
+                    b.append(Arrays.toString(image)).append(LINE_FEED);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
         }
         b.append("--").append(boundary).append("--").append(LINE_FEED);
         b.append(LINE_FEED);
