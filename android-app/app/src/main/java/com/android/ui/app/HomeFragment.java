02199e2f219be267363ed7a5997faff9b9468d0f
sergiu <sergiumer@gmail.com>
2023-02-24 00:16:26 +0000

    Location functionality for home screen.

--- android-app/app/src/main/java/com/android/ui/app/HomeFragment.java
+++ android-app/app/src/main/java/com/android/ui/app/HomeFragment.java
@@ -1,36 +1,76 @@
 package com.android.ui.app;
 
+import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.android.AppActivity;
 import com.android.LoginAndRegisterActivity;
 import com.android.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Button locationButton;
+    private String cityName = null;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        locationButton = binding.locationButton;
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+        fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null)
+                .addOnSuccessListener(getActivity(), location -> {
+                    if (location != null) {
+                        Geocoder geocoder = new Geocoder(getActivity());
+                        try {
+                            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            cityName = addresses.get(0).getLocality();
+                            Toast.makeText(getActivity(), cityName, Toast.LENGTH_SHORT).show();
+                            locationButton.setText(cityName);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         binding.goToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +81,29 @@ public class HomeFragment extends Fragment {
 //                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+        binding.locationButton.setOnClickListener(view1 -> {
+            // Get the user's location and set the location button to the nearest city.
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                locationPermissionRequest.launch(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                });
+            }
+            fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null)
+                    .addOnSuccessListener(getActivity(), location -> {
+                        if (location != null) {
+                            Geocoder geocoder = new Geocoder(getActivity());
+                            try {
+                                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                cityName = addresses.get(0).getLocality();
+                                Toast.makeText(getActivity(), cityName, Toast.LENGTH_SHORT).show();
+                                locationButton.setText(cityName);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+            });
     }
 
     @Override
@@ -49,4 +112,20 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
+    private final ActivityResultLauncher<String[]> locationPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts
+                            .RequestMultiplePermissions(), result -> {
+                        Boolean fineLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        Boolean coarseLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                        if (fineLocationGranted != null && fineLocationGranted) {
+                            // Location permission granted fully.
+                        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                            Toast.makeText(getActivity(), "Please grant precise permissions before using the main app.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), "Please grant all location permissions before using the main app.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
 }
