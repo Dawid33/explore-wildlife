3cf2331d4848a5eaea5d1e2f402d3e56b73c23c7
TheCaister <edisoncai2001@gmail.com>
2023-04-05 23:34:06 +0100

    returning lat and long from posts

--- backend/src/posts.py
+++ backend/src/posts.py
@@ -175,13 +175,19 @@ def like_post(post):
 def get_posts():
     conn = db.get_db()
     cur = conn.cursor()
+    # cur.execute(
+    #     "SELECT post_id, content, created_by, created_at, has_images FROM app.posts  where created_at in (SELECT max(created_at) FROM app.posts GROUP BY created_at) order by created_at desc limit 10")
+
     cur.execute(
-        "SELECT post_id, content, created_by, created_at, has_images FROM app.posts  where created_at in (SELECT max(created_at) FROM app.posts GROUP BY created_at) order by created_at desc limit 10")
+        "SELECT post_id, title, content, created_by, created_at, has_images, ST_X(location::geometry), "
+        "ST_Y(location::geometry) FROM app.posts  where created_at in (SELECT max(created_at) FROM app.posts GROUP BY "
+        "created_at) order by created_at desc limit 10")
+
     result = cur.fetchall()
     posts = []
     for raw_post in result:
-        post = {"post_id": raw_post[0], "content": raw_post[1], "created_by": raw_post[2], "created_at": raw_post[3]}
-        if bool(raw_post[4]):
+        post = {"post_id": raw_post[0], "title": raw_post[1], "content": raw_post[2], "created_by": raw_post[3], "created_at": raw_post[4], "longitude": raw_post[6], "latitude": raw_post[7]}
+        if bool(raw_post[5]):
             cur.execute("SELECT post_id, image_id FROM app.post_images WHERE post_id = %s", [post["post_id"]])
             result = cur.fetchall()
             post["images"] = [x[1] for x in result]
